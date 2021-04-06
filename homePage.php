ac762d929914e60f0e32d7f3441e6ee12b92daea
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2021-04-06 21:09:11 +0200

    Tworzenie konta uzytkownika

--- homePage.php
+++ homePage.php
@@ -4,7 +4,105 @@
     if((isset($_SESSION['isLoggedIn'])) && ($_SESSION['isLoggedIn'] == true)){
         header('Location: userProfile.php');
         exit();
+    }else if((isset($_POST['name'])) && (isset($_POST['surname'])) && (isset($_POST['emailRegister'])) && (isset($_POST['phone_number']))
+        && (isset($_POST['password1'])) && (isset($_POST['password2']))){
+        $validationCompleted = true;
+
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $emailRegister = $_POST['emailRegister'];
+        $emailRegisterSafety = filter_var($emailRegister,FILTER_SANITIZE_EMAIL);
+        $phone_number = $_POST['phone_number'];
+        $password1 = $_POST['password1'];
+        $password2 = $_POST['password2'];
+
+        if(strlen($name) < 3 || strlen($name) > 20){
+            $validationCompleted = false;
+            $_SESSION['err_name'] = '<span style = "color:#ff0000">Name must contain 3 to 20 letters</span><br>';
+        }
+
+        if(ctype_alpha($name) == false){
+            $validationCompleted = false;
+            $_SESSION['err_name'] = '<span style = "color:#ff0000">Name must contain only letters</span><br>';
+        }
+
+        if(strlen($surname) < 3 || strlen($surname) > 30){
+            $validationCompleted = false;
+            $_SESSION['err_surname'] = '<span style = "color:#ff0000">Surname must contain 3 to 30 letters</span><br>';
+        }
+
+        if(ctype_alpha($surname) == false){
+            $validationCompleted = false;
+            $_SESSION['err_surname'] = '<span style = "color:#ff0000">Surname must contain only letters</span><br>';
+        }
+
+        if(filter_var($emailRegisterSafety,FILTER_VALIDATE_EMAIL) == false || $emailRegisterSafety != $emailRegister){
+            $validationCompleted = false;
+            $_SESSION['err_email'] = '<span style = "color:#ff0000">Please type in correct e-mail</span><br>';
+        }
+
+        if(strlen($password1) < 8 || strlen($password1) > 30){
+            $validationCompleted = false;
+            $_SESSION['err_psswd'] = '<span style = "color:#ff0000">Password must contain 8 to 30 letters</span><br>';
+        }
+
+        if($password1 != $password2){
+            $validationCompleted = false;
+            $_SESSION['err_psswd'] = '<span style = "color:#ff0000">Password are not identical</span><br>';
+        }
+
+        $password1_hash = password_hash($password1,PASSWORD_DEFAULT);
+
+        if(!isset($_POST['regulations'])){
+            $validationCompleted = false;
+            $_SESSION['err_regulations'] = '<span style = "color:#ff0000">I am over 18 age, and I agree to the Terms</span><br>';
+        }
+
+        //phone number validacja
+
+        require_once "dataBaseConnector.php";
+
+        mysqli_report(MYSQLI_REPORT_STRICT);
+
+        try{
+            $connection = new mysqli($host, $db_user, $db_password, $db_name);
+
+            if($connection -> connect_errno != 0){
+                throw new Exception(mysqli_connect_error());
+            } else{
+                //whether that email exist in db
+                $result = $connection -> query("SELECT id_użytkownika FROM użytkownicy WHERE adres_email = '$emailRegister'");
+
+                if(!$result) throw new Exception($connection -> error);
+
+                if($result -> num_rows > 0){
+                    $validationCompleted = false;
+                    $_SESSION['err_email'] = '<span style = "color:#ff0000">User with that e-mail already exist</span><br>';
+                }
+                //whether that phone number exist in db
+                $result = $connection -> query("SELECT id_użytkownika FROM użytkownicy WHERE nr_telefonu = '$phone_number'");
+
+                if(!$result) throw new Exception($connection -> error);
+
+                if($result -> num_rows > 0){
+                    $validationCompleted = false;
+                    $_SESSION['err_phone'] = '<span style = "color:#ff0000">User with that phone number already exist</span><br>';
+                }
+
+                $connection -> close();
+            }
+        }catch (Exception $exception){
+            echo "Error: ".$exception;
+        }
+
+        if($validationCompleted == true){
+            //insert
+        }else{
+            //error
+        }
     }
+
+
 ?>
 
 <!DOCTYPE HTML>
@@ -43,7 +141,7 @@
             <input class="text" type="text"  placeholder="Enter Email" name="email" required><br>
             <label class="napis"><b>Password</b></label><br>
             <input class="text" type="password" placeholder="Enter Password" name="password" required><br>
-            <?php if(isset($_SESSION['error'])) echo $_SESSION['error']; ?>
+            <?php if(isset($_SESSION['error'])) echo $_SESSION['error'];?>
             <button type="submit" class="btn" id="login">Login</button>
         </form>
           <button class="btn" id="close" onclick="FormVis()">Close</button>
@@ -53,21 +151,30 @@
 
       <div class="new-account" id="create-account-form" style="display: none; position: fixed;">
         <div id="form-div2">
-          <form action="createAcc.php" method = post>
+          <form method = post>
 
             <h1 style="font-size: 20px;">Register<h1></h1>
             <label class="napis">Name</label><br>
-            <input class="text" type="text" placeholder="Enter Name" name="name"><br>
+            <input class="text" type="text" placeholder="Enter Name" name="name" required><br>
+                <?php if(isset($_SESSION['err_name'])){ echo $_SESSION['err_name']; unset($_SESSION['err_name']);} ?>
             <label class="napis">Surname</label><br>
-            <input class="text" type="text" placeholder="Enter Surname" name="surname"><br>
+            <input class="text" type="text" placeholder="Enter Surname" name="surname" required><br>
+                <?php if(isset($_SESSION['err_surname'])){ echo $_SESSION['err_surname']; unset($_SESSION['err_surname']);} ?>
             <label class="napis">Email</label><br>
-            <input class="text" type="text" placeholder="Enter Email" name="email"><br>
+            <input class="text" type="text" placeholder="Enter Email" name="emailRegister" required><br>
+                <?php if(isset($_SESSION['err_email'])){ echo $_SESSION['err_email']; unset($_SESSION['err_email']);} ?>
             <label class="napis">Phone Number</label><br>
-            <input class="text" type="text" placeholder="Enter Phone Number" name="phone_number"><br>
+            <input class="text" type="text" placeholder="Enter Phone Number" name="phone_number" required><br>
+                <?php if(isset($_SESSION['err_phone'])){ echo $_SESSION['err_phone']; unset($_SESSION['err_phone']);} ?>
             <label class="napis">Password</label><br>
-            <input class="text" type="text" placeholder="Enter Password"name="password1"><br>
+            <input class="text" type="text" placeholder="Enter Password"name="password1" required><br>
+                <?php if(isset($_SESSION['err_psswd'])){ echo $_SESSION['err_psswd']; unset($_SESSION['err_psswd']);} ?>
             <label class="napis">Repeat password</label><br>
-            <input class="text" type="text" placeholder="Repeat Password" name="password2"><br>
+            <input class="text" type="text" placeholder="Repeat Password" name="password2" required><br>
+            <label>
+                <input type="checkbox" name="regulations"/> I have read and agree to the Terms of Service.<br>
+            </label>
+                <?php if(isset($_SESSION['err_regulations'])){ echo $_SESSION['err_regulations']; unset($_SESSION['err_regulations']);} ?>
             <button type="submit" class="btn" id="create">Create</button>
 
           </form>
--- logIn.php
+++ logIn.php
@@ -10,39 +10,44 @@
     $connection = @new mysqli($host, $db_user, $db_password, $db_name);
 
     if($connection -> connect_errno != 0){
-        echo "Error: ".$connection -> connect_errno. "Description: ".$connection -> connect_errno;
+        echo "Error: ".$connection -> connect_errno;
     } else {
         $email = $_POST['email'];
         $password = $_POST['password'];
 
         $email = htmlentities($email,ENT_QUOTES, "UTF-8");
-        $password = htmlentities($password,ENT_QUOTES, "UTF-8");
 
-
-        $sql_query = "SELECT * FROM użytkownicy WHERE adres_email = '$email' AND haslo = '$password'";
-
-        if($result = $connection -> query(
-            sprintf("SELECT * FROM użytkownicy WHERE adres_email = '%s' AND haslo = '%s'",
-            mysqli_real_escape_string($connection,$email),
-            mysqli_real_escape_string($connection,$password))
-        )){
+        if($result = @$connection -> query(
+            sprintf("SELECT * FROM użytkownicy WHERE BINARY adres_email = '%s'",
+            mysqli_real_escape_string($connection,$email))))
+        {
             $correct_users = $result -> num_rows;
             if($correct_users == 1){
-                $_SESSION['isLoggedIn'] = true;
-
                 $data = $result -> fetch_assoc();
-                $_SESSION['id_użytkownika'] = $data['id_użytkownika'];
-                $_SESSION['imię'] = $data['imię'];
-                $_SESSION['nazwisko'] = $data['nazwisko'];
-                $_SESSION['nr_telefonu'] = $data['nr_telefonu'];
-                $_SESSION['id_portfela'] = $data['id_portfela'];
-                $_SESSION['adres_email'] = $data['adres_email'];
 
-                unset($_SESSION['error']);
-                $result -> free();
+                if(password_verify($password,$data['haslo'])) {
 
-                header('Location: userProfile.php');
-            }else{
+                    $_SESSION['isLoggedIn'] = true;
+
+                    $_SESSION['id_użytkownika'] = $data['id_użytkownika'];
+                    $_SESSION['imię'] = $data['imię'];
+                    $_SESSION['nazwisko'] = $data['nazwisko'];
+                    $_SESSION['nr_telefonu'] = $data['nr_telefonu'];
+                    $_SESSION['id_portfela'] = $data['id_portfela'];
+                    $_SESSION['adres_email'] = $data['adres_email'];
+
+                    unset($_SESSION['error']);
+                    $result->free();
+
+                    header('Location: userProfile.php');
+                }else {
+                    echo "Bledne dane do logowania!";
+
+                    $_SESSION['error'] = '<span style = "color:#ff0000">Błędny login lub hasło!</span>';
+                    header('Location: homePage.php#log-popup');
+                }
+
+            }else {
                 echo "Bledne dane do logowania!";
 
                 $_SESSION['error'] = '<span style = "color:#ff0000">Błędny login lub hasło!</span>';
