a0cb1c5d0eb00f46d587f4fe211859231dfe0c11
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2021-05-27 14:25:23 +0200

    bug fixed

--- testAPI.php
+++ testAPI.php
@@ -1,68 +1,17 @@
-//ajax
-if (e.options[e.selectedIndex].id == 'myWallet') {
-document.getElementById('amountInputBuy').value = e.options[e.selectedIndex].id;
-}else{
-var data1 = "xd";
-$.ajax({
-method: "POST",
-url: "home.php",
-data: { data1: data1},
-}).done(function(html){
-alert("success!");
+<?php
 
-document.getElementById('amountInputBuy').value = <?php
+$chi = curl_init();
+$urli = "https://api.coingecko.com/api/v3/coins/{$_SESSION['xd']}/market_chart?vs_currency=eur&days=1&interval=daily";
+curl_setopt($chi, CURLOPT_URL, $urli);
+curl_setopt($chi, CURLOPT_RETURNTRANSFER, true);
+$resp = curl_exec($chi);
+$priceDayBefore = json_decode($resp, true);
 
-if(isset($_POST['data1'])){
-    echo json_encode("xd");
-}else{
-    echo json_encode("ta nie dziala");
-}
-?>;
+curl_close($chi);
 
-}).fail(function(html){
-alert("nah!");
-});
-}
+echo $priceDayBefore['prices'][0][1];
 
+// dzisiaj echo $priceDayBefore['prices'][1][1];
+// wczoraj echo $priceDayBefore['prices'][0][1];
 
-
-//inny sposob z cookie
-var e = document.getElementById('toPay');
-var a = document.getElementById('toBuy');
-createCookie("id",e.options[e.selectedIndex].id,10);
-
-document.getElementById('amountInputBuy').value = <?php
-
-if(isset($_COOKIE["id"])){
-    echo json_encode($_COOKIE["id"]);
-    setcookie("id", time() - 3600);
-    //echo json_encode("xd");
-}else{
-    echo json_encode("nie dziala");
-}
-?>;
-
-
-$('#buyForm').submit(function(e) {
-e.preventDefault();
-$.ajax({
-type: "POST",
-url: 'home.php',
-data: $(this).serialize(),
-success: function(response)
-{
-var jsonData = JSON.parse(response);
-
-// user is logged in successfully in the back-end
-// let's redirect
-if (jsonData.success == "1")
-{
-alert('xd');
-}
-else
-{
-alert('Invalid Credentials!');
-}
-}
-});
-});
+?>
