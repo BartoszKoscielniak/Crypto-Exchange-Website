70e9ca928edc35dce0faec954830d385203fc2bc
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2022-03-10 21:15:15 +0100

    home.php

--- Views/home.php
+++ Views/home.php
@@ -162,7 +162,7 @@ if ((isset($_SESSION['isLoggedIn'])) && ($_SESSION['isLoggedIn'] == true)) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="homePageStyle.css">
+    <link rel="stylesheet" href="stylesheet/homePageStyle.css">
 </head>
 
 <body>
